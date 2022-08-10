e777819408f4bd1a2871f19d1f8b4d80cff7e902
Juan Pablo Mosquera <jp@xMacBookAir.local>
2022-08-10 02:39:46 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+import os
+import yaml
+
+from aws_cdk import (
+    App,
+    Tags,
+    Environment
+)
+
+from aws_cdk_py.aws_cdk_py_stack import AwsCdkPyStack
+from aws_cdk_py.pipeline_stack import Pipeline
+
+
+with open('./parameters.yml') as file:
+    parameters = yaml.load(file, Loader=yaml.FullLoader)
+
+with open('./tags.yml') as file:
+    tags = yaml.load(file, Loader=yaml.FullLoader)
+
+# map parameters to be passed to the stacks
+props = {
+    'namespace': parameters['namespace']
+}
+
+# Define enviroment to be used on diferent stacks
+env_=Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION'))
+
+app = App()
+
+# Set tags to all resources deployed
+for tag_name, tag_value in tags.items():
+    Tags.of(app).add(
+        tag_name,
+        tag_value
+    )
+
+# Base stack
+AwsCdkPyStack(app, f"{props['namespace']}", props, env=env_)
+
+# Pipeline stack
+Pipeline(app, f"{props['namespace']}-PipelineStack", AwsCdkPyStack.outputs, env=env_)
+
+app.synth()
---
+++ aws_cdk_py/pipeline_stack.py
@@ -0,0 +1,26 @@
+from aws_cdk import (
+    App,
+    Stack
+)
+from aws_cdk.pipelines import (
+    ShellStep,
+    CodePipeline,
+    CodePipelineSource
+)
+
+
+class Pipeline(Stack):
+    def __init__(self, app: App, id: str, props, **kwargs) -> None:
+        super().__init__(app, id, **kwargs)
+
+        source   = CodePipelineSource.git_hub("OWNER/REPO", "main")
+
+        synth_step = ShellStep("Synth", 
+                        input=source,
+                        commands=["npm install -g aws-cdk",
+                        "python -m pip install -r requirements.txt",
+                        "cdk synth"])
+
+        pipeline   = CodePipeline(self, f"{props['namespace']}-Pipeline", 
+                        pipeline_name=f"{props['namespace']}-Pipeline",
+                        synth=synth_step)
