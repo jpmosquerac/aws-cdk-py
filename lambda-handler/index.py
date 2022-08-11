6c34c35eedc028866503f39b6ed03c7974cba6ab
Juan Pablo Mosquera <jp@xMacBookAir.local>
2022-08-11 02:28:06 -0500

    quick fix

--- lambda-handler/index.py
+++ lambda-handler/index.py
@@ -24,7 +24,7 @@ def handler(event, context):
                 'Access-Control-Allow-Origin': '*',
                 'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
             },
-            'body': json.dumps('%s',result)
+            'body': json.dumps('{result}')
         }
     return {
         'statusCode': 500,
