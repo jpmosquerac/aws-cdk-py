84b0547f92a3c12b1929c990ea2e8b139088402e
Juan Pablo Mosquera <jp@xMacBookAir.local>
2022-08-11 02:10:00 -0500

    Making it even more simple

--- aws_cdk_py/serverless_backend_stack.py
+++ aws_cdk_py/serverless_backend_stack.py
@@ -15,40 +15,20 @@ class ServerlessBackendStack(Stack):
     def __init__(self, app: App, id: str, props, **kwargs) -> None:
         super().__init__(app, id, **kwargs)
 
-        bucket = aws_s3.Bucket(
-            self, 'ProjectBucket',
-            bucket_name=f"{props['namespace'].lower()}-{Aws.ACCOUNT_ID}",
-            versioned=True,
-            removal_policy=RemovalPolicy.DESTROY,
-            auto_delete_objects=True
-        )
-
-        table = aws_dynamodb.Table(self, id='dynamoTable', table_name='formmetadata', partition_key=aws_dynamodb.Attribute(
-            name='userid', type=aws_dynamodb.AttributeType.STRING)) #change primary key here
-
-        lambda_ = aws_lambda.Function(self, id='lambdafunction', function_name="formlambda", runtime=aws_lambda.Runtime.PYTHON_3_7,
+        lambda_ = aws_lambda.Function(self, id='lambdafunction', function_name="lambdafunction", runtime=aws_lambda.Runtime.PYTHON_3_7,
                                      handler='index.handler',
                                      code=aws_lambda.Code.from_asset(
                                          os.path.join("./", "lambda-handler")),
-                                     environment={
-                                         'bucket': bucket.bucket_name,
-                                         'table': table.table_name
-                                     }
                                      )
 
-        bucket.grant_read_write(lambda_)
-
-        table.grant_read_write_data(lambda_)
-
         api = aws_apigateway.LambdaRestApi(
-            self, id='lambdaapi', rest_api_name='formapi', handler=lambda_, proxy=True)
+            self, id='lambdaapi', rest_api_name='lambdaapi', handler=lambda_, proxy=True)
 
-        postData = api.root.add_resource("form")
+        postData = api.root.add_resource("sum")
         
         postData.add_method("POST")
 
         self.output_props = props.copy()
-        self.output_props['bucket'] = bucket
         self.output_props['lambda'] = lambda_
 
     @property
